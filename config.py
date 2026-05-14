d0c286c7cfd21aa3f406f8199573ec9b9bf0ba72
aminjonoff1-prog <aminjonoff1@gmail.com>
2026-05-14 10:52:35 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,6 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
-HF_API_KEY = os.getenv("HF_API_KEY")
 FREE_LIMIT = int(os.getenv("FREE_LIMIT", 5))
 
 print("Kofiglar xavfsiz yuklandi OK")
