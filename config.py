607b39f190b6069fc544703f069b7de3a6140caf
aminjonoff1-prog <aminjonoff1@gmail.com>
2026-05-16 15:50:38 +0500

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 FREE_LIMIT = int(os.getenv("FREE_LIMIT", 5))
 
 print("Kofiglar xavfsiz yuklandi OK")
