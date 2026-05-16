e2401a58ba6918c949e34fd2cc63f6075723ffc2
aminjonoff1-prog <aminjonoff1@gmail.com>
2026-05-16 16:18:45 +0500

    Update config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+ADMIN_ID = int(os.getenv("ADMIN_ID", 7168047390))
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 STABILITY_API_KEY = os.getenv("STABILITY_API_KEY")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
