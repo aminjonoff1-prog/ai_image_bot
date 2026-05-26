fc785fe462da22984ba0116c9149143c879019f2
aminjonoff1-prog <aminjonoff1@gmail.com>
2026-05-26 10:58:42 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,14 +1,30 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_menu():
-    # Tugmalarni qatorlarga ajratib chiqamiz
-    kb = [
-        [KeyboardButton(text="🎨 Logo"), KeyboardButton(text="🖼 Realistik")],
-        [KeyboardButton(text="📱 Avatar"), KeyboardButton(text="🏠 Interyer")],
-        [KeyboardButton(text="🌄 Landscape"), KeyboardButton(text="🏢 Reklama Banneri")],
-        [KeyboardButton(text="🖥 UI/UX Web Dizayn"), KeyboardButton(text="🏢 3D Arxitektura")],
-        [KeyboardButton(text="💎 Brending"), KeyboardButton(text="🎮 Konsept Art")]
+    keyboard = [
+        [
+            KeyboardButton(text="🎨 Logo"),
+            KeyboardButton(text="🖼 Realistik"),
+        ],
+        [
+            KeyboardButton(text="📱 Avatar"),
+            KeyboardButton(text="🏠 Interyer"),
+        ],
+        [
+            KeyboardButton(text="🌄 Landscape"),
+            KeyboardButton(text="🖥 UI/UX Web Dizayn"),
+        ],
+        [
+            KeyboardButton(text="🏢 3D Arxitektura"),
+            KeyboardButton(text="💎 Brending"),
+        ],
+        [
+            KeyboardButton(text="🎮 Konsept Art"),
+            KeyboardButton(text="🏢 Reklama Banneri"),
+        ],
     ]
-    
-    # resize_keyboard=True tugmalar ekranning yarmini egallab olmasligi uchun kerak
-    return ReplyKeyboardMarkup(keyboard=kb, resize_keyboard=True)
+
+    return ReplyKeyboardMarkup(
+        keyboard=keyboard,
+        resize_keyboard=True
+    )
