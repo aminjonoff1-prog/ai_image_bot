dd85d376fa7b1560692958812752a66ef41641f2
aminjonoff1-prog <aminjonoff1@gmail.com>
2026-05-26 10:43:18 +0500

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -5,7 +5,7 @@ def main_menu():
     kb = [
         [KeyboardButton(text="🎨 Logo"), KeyboardButton(text="🖼 Realistik")],
         [KeyboardButton(text="📱 Avatar"), KeyboardButton(text="🏠 Interyer")],
-        [KeyboardButton(text="🌄 Landscape"), KeyboardButton(text="📊 Prezentatsiya")],
+        [KeyboardButton(text="🌄 Landscape")],
         [KeyboardButton(text="🖥 UI/UX Web Dizayn"), KeyboardButton(text="🏢 3D Arxitektura")],
         [KeyboardButton(text="💎 Brending"), KeyboardButton(text="🎮 Konsept Art")],
         [KeyboardButton(text="🏢 Reklama Banneri"), KeyboardButton(text="📚 Kurs Ishi")]
